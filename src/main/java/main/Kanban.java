64acf40b7b5c1638e11cbffbf608ca4680668ccd
YeriAddict <denis.leang@gmail.com>
2022-10-23 00:35:04 +0200

    Hello world commit

--- src/main/java/main/Kanban.java
+++ src/main/java/main/Kanban.java
@@ -2,12 +2,18 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
-public class Kanban {
+public class Kanban implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Kanban.class, args);
+	}
+	
+	@Override
+	public void run(String ...args) throws Exception {
+		System.out.println("Hello World!");
 	}
 
 }
