fe6223cf4f62c50e3ca5b04a028b0ef83eb21982
YeriAddict <denis.leang@gmail.com>
2022-10-22 21:10:24 +0200

    Modified main package

--- src/main/java/main/Kanban.java
+++ src/main/java/main/Kanban.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KanbanApplication {
+public class Kanban {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KanbanApplication.class, args);
+		SpringApplication.run(Kanban.class, args);
 	}
 
 }
