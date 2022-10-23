b46d846f8ee5dcfdd6fddf0c87fc1af1b4ea7eb2
YeriAddict <denis.leang@gmail.com>
2022-10-23 23:34:38 +0200

    Modified main

--- src/main/java/leang/main/Kanban.java
+++ src/main/java/leang/main/Kanban.java
@@ -2,18 +2,12 @@ package leang.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
-public class Kanban implements CommandLineRunner{
-
+public class Kanban{
+    
 	public static void main(String[] args) {
 		SpringApplication.run(Kanban.class, args);
 	}
 	
-	@Override
-	public void run(String ...args) throws Exception {
-		System.out.println("Hello World!");
-	}
-
 }
