7bcd126fb3d589dd020888ca9554ea5519f1f272
YeriAddict <denis.leang@gmail.com>
2022-10-23 03:18:55 +0200

    Changed Date to LocalDate (better)

--- src/main/java/leang/entities/ChangeLog.java
+++ src/main/java/leang/entities/ChangeLog.java
@@ -1,6 +1,6 @@
 package leang.entities;
 
-import java.sql.Date;
+import java.time.LocalDate;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,7 +12,7 @@ public class ChangeLog {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
-    private Date occured;
+    private LocalDate occured;
     
     public ChangeLog() {
     }
@@ -20,6 +20,6 @@ public class ChangeLog {
     public Long getId() {return id;}
     public void setId(Long id) {this.id = id;}
 
-    public Date getOccured() {return occured;}
-    public void setOccured(Date occured) {this.occured = occured;}
+    public LocalDate getOccured() {return occured;}
+    public void setOccured(LocalDate occured) {this.occured = occured;}
 }
