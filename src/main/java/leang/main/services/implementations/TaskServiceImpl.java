189e87e99f6edb6a650801232c220400a3613e8b
YeriAddict <denis.leang@gmail.com>
2022-10-24 10:45:10 +0200

    Created findAllTasks method

---
+++ src/main/java/leang/main/services/implementations/TaskServiceImpl.java
@@ -0,0 +1,20 @@
+package leang.main.services.implementations;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import leang.main.entities.Task;
+import leang.main.repositories.TaskRepository;
+
+@Service
+public class TaskServiceImpl {
+
+    @Autowired
+    private TaskRepository taskRepository;
+    
+    public Collection<Task> findAllTasks(){
+        return this.taskRepository.findAll();
+    }
+}
