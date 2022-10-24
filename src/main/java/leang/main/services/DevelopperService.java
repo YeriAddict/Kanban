3cfaf899f116cfa18ba74ba058d3f98e0086be1e
YeriAddict <denis.leang@gmail.com>
2022-10-24 10:29:05 +0200

    Corrected services

--- src/main/java/leang/main/services/DevelopperService.java
+++ src/main/java/leang/main/services/DevelopperService.java
@@ -2,11 +2,8 @@ package leang.main.services;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
 import leang.main.entities.Developer;
 
-@Service
 public interface DevelopperService {
     
     public List<Developer> findAllDevelopers();
--- src/main/java/leang/main/services/TaskService.java
+++ src/main/java/leang/main/services/TaskService.java
@@ -2,11 +2,8 @@ package leang.main.services;
 
 import java.util.Collection;
 
-import org.springframework.stereotype.Service;
-
 import leang.main.entities.Task;
 
-@Service
 public interface TaskService {
 
     public Collection<Task> findAllTasks();
