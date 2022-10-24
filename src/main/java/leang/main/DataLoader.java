2246ac46275938b64e54c06d8981396fb1ce12e8
YeriAddict <denis.leang@gmail.com>
2022-10-24 19:25:02 +0200

    Added task status to database test

--- src/main/java/leang/main/DataLoader.java
+++ src/main/java/leang/main/DataLoader.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 
 import leang.main.entities.Developer;
 import leang.main.entities.Task;
+import leang.main.entities.TaskStatus;
 import leang.main.repositories.ChangeLogRepository;
 import leang.main.repositories.DeveloperRepository;
 import leang.main.repositories.TaskRepository;
@@ -42,7 +43,6 @@ public class DataLoader implements CommandLineRunner {
         Developer wendy = new Developer("Seung-wan", "Son", "Wendy", "rv_wendy@gmail.com", LocalDate.of(1994, Month.FEBRUARY, 21));
         Developer joy = new Developer("Soo-young", "Park", "Joy", "rv_joy@gmail.com", LocalDate.of(1996, Month.SEPTEMBER, 3));
         Developer yeri = new Developer("Yerim", "Kim", "Yeri", "rv_yeri@gmail.com", LocalDate.of(1999, Month.MARCH, 5));
-        
         developerRepository.save(irene);
         developerRepository.save(seulgi);
         developerRepository.save(wendy);
@@ -53,5 +53,14 @@ public class DataLoader implements CommandLineRunner {
         Task task_two = new Task("Queendom 2021", 0, 0, LocalDate.of(2021, Month.AUGUST, 21));
         taskRepository.save(task_one);
         taskRepository.save(task_two);
+        
+        TaskStatus status_one = new TaskStatus("WORK_TO_DO");
+        TaskStatus status_two = new TaskStatus("WORK_ON_PROGRESS");
+        TaskStatus status_three = new TaskStatus("WORK_TO_TEST");
+        TaskStatus status_four = new TaskStatus("WORK_DONE");
+        taskStatusRepository.save(status_one);
+        taskStatusRepository.save(status_two);
+        taskStatusRepository.save(status_three);
+        taskStatusRepository.save(status_four);
     }
 }
