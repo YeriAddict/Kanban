2ab669479487a1afe2bdf44cef3e178009e969ed
YeriAddict <denis.leang@gmail.com>
2022-10-24 20:03:56 +0200

    Created constants

--- src/main/java/leang/main/structs/Status.java
+++ src/main/java/leang/main/structs/Status.java
@@ -5,9 +5,9 @@ public class Status {
     public static String STATUS_TWO_LABEL = "WORK_ON_PROGRESS"; 
     public static String STATUS_THREE_LABEL = "WORK_TO_TEST"; 
     public static String STATUS_FOUR_LABEL = "WORK_DONE"; 
-    public static Long TYPE_ONE_ID = 1L; 
-    public static Long TYPE_TWO_ID = 2L;
-    public static Long TYPE_THREE_ID = 3L; 
-    public static Long TYPE_FOUR_ID = 4L; 
+    public static Long STATUS_ONE_ID = 1L; 
+    public static Long STATUS_TWO_ID = 2L;
+    public static Long STATUS_THREE_ID = 3L; 
+    public static Long STATUS_FOUR_ID = 4L; 
 
 }
