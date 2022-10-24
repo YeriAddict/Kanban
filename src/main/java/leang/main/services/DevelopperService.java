5bd7a8c6e61478c405a505a1bf147a6ca0da1824
YeriAddict <denis.leang@gmail.com>
2022-10-24 16:47:10 +0200

    Added service for developer

--- src/main/java/leang/main/services/DevelopperService.java
+++ src/main/java/leang/main/services/DevelopperService.java
@@ -2,8 +2,11 @@ package leang.main.services;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import leang.main.entities.Developer;
 
+@Service
 public interface DevelopperService {
     
     public List<Developer> findAllDevelopers();
