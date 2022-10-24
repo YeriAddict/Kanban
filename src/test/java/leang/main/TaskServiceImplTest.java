82bdcb98fbadc5cb2a33ee2278713f35a9f7b597
YeriAddict <denis.leang@gmail.com>
2022-10-24 19:32:16 +0200

    Added tests for find task status

--- src/test/java/leang/main/TaskServiceImplTest.java
+++ src/test/java/leang/main/TaskServiceImplTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import leang.main.entities.Task;
+import leang.main.entities.TaskStatus;
 import leang.main.services.TaskService;
 
 @SpringBootTest
@@ -30,5 +31,18 @@ public class TaskServiceImplTest {
     public void findTaskTest() {
         Task task = this.taskService.findTask(1);
         Assert.assertEquals("ReVe Festival 2022", task.getTitle());
+    }
+    
+    @Test
+    public void findAllTaskStatusTest() {
+        Collection<TaskStatus> taskStatusListTest = this.taskService.findAllTaskStatus();
+        Assert.assertEquals(4, taskStatusListTest.size(), 0);
+        Assert.assertNotEquals(5, taskStatusListTest.size(), 0);
+    }
+    
+    @Test
+    public void findTaskStatusTest() {
+        TaskStatus taskStatus = this.taskService.findTaskStatus(1);
+        Assert.assertEquals("WORK_TO_DO", taskStatus.getLabel());
     }
 }
