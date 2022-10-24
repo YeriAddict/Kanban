a5c54e441e6e0966cb7bd27441c297402dce62e7
YeriAddict <denis.leang@gmail.com>
2022-10-24 19:18:23 +0200

    Added find methods for task status and type

--- src/main/java/leang/main/services/TaskService.java
+++ src/main/java/leang/main/services/TaskService.java
@@ -3,9 +3,18 @@ package leang.main.services;
 import java.util.Collection;
 
 import leang.main.entities.Task;
+import leang.main.entities.TaskStatus;
+import leang.main.entities.TaskType;
 
 public interface TaskService {
 
     public Collection<Task> findAllTasks();
     public Task findTask(long id);
+    public Collection<TaskStatus> findAllTaskStatus();
+    public TaskStatus findTaskStatus(long id);
+    public Collection<TaskType> findAllTaskTypes();
+    public TaskType findTaskType(long id);
+    public Task moveRightTask(Task task);
+    public Task moveLeftTask(Task task);
+    
 }
--- src/main/java/leang/main/services/implementations/TaskServiceImpl.java
+++ src/main/java/leang/main/services/implementations/TaskServiceImpl.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import leang.main.entities.Task;
+import leang.main.entities.TaskStatus;
+import leang.main.entities.TaskType;
 import leang.main.repositories.TaskRepository;
+import leang.main.repositories.TaskStatusRepository;
+import leang.main.repositories.TaskTypeRepository;
 import leang.main.services.TaskService;
 
 @Service
@@ -14,6 +18,10 @@ public class TaskServiceImpl implements TaskService {
 
     @Autowired
     private TaskRepository taskRepository;
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
+    @Autowired
+    private TaskTypeRepository taskTypeRepository;
     
     public Collection<Task> findAllTasks(){
         return this.taskRepository.findAll();
@@ -22,5 +30,28 @@ public class TaskServiceImpl implements TaskService {
     public Task findTask(long id) {
         return this.taskRepository.getReferenceById(id);
     }
+    
+    public Collection<TaskStatus> findAllTaskStatus(){
+        return this.taskStatusRepository.findAll();
+    }
+    
+    public TaskStatus findTaskStatus(long id) {
+        return this.taskStatusRepository.getReferenceById(id);
+    }
+    
+    public Collection<TaskType> findAllTaskTypes(){
+        return this.taskTypeRepository.findAll();
+    }
+    
+    public TaskType findTaskType(long id) {
+        return this.taskTypeRepository.getReferenceById(id);
+    }
+    
+    public Task moveRightTask(Task task) {
+        return new Task();
+    }
 
+    public Task moveLeftTask(Task task) {
+        return new Task();
+    }
 }
