6a515e8d5305a0dd68d2ab78a5ea826c023b43b5
YeriAddict <denis.leang@gmail.com>
2022-10-24 08:56:23 +0200

    Added test for addDevelopper

---
+++ src/test/java/leang/main/TaskTest.java
@@ -0,0 +1,21 @@
+package leang.main;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import leang.main.entities.Developer;
+import leang.main.entities.Task;
+
+@SpringBootTest
+class TaskTest {
+    @Test
+    public void addDevelopperTest() {
+        Developer developperTest = new Developer();
+        Task taskTest = new Task();
+        taskTest.addDeveloper(developperTest);
+        Assert.assertEquals(1, taskTest.getDevelopers().size(),0);
+    }
+}
